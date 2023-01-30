1cf6ecce3c55ec8ed166bd29c702b886437a3466
singhutsav5502 <singhutsavofficial@gmail.com>
2023-01-30 14:58:32 +0530

    link CSS files

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,7 +1,14 @@
-function ExpenseItem(){
-    return(
-        <h2>Expense Item !</h2>
-    )
+import './ExpenseItem.css';
+function ExpenseItem() {
+    return (
+        <div className="expense-item">
+            <div>March 28th 2021</div>
+            <div  className="expense-item__description">
+                <h2>Car Insurance</h2>
+                <div className="expense-item__price">$256.79</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
