97754cd2be16d74ad44289368b93bac829e8d782
singhutsav5502 <singhutsavofficial@gmail.com>
2023-01-30 15:11:13 +0530

    Adding dynamic data

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,11 +1,14 @@
 import './ExpenseItem.css';
 function ExpenseItem() {
+    const expenseDate = new Date(2023,0,1);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 256.79;
     return (
         <div className="expense-item">
-            <div>March 28th 2021</div>
+            <div>{expenseDate.toISOString()}</div>
             <div  className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$256.79</div>
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">${expenseAmount}</div>
             </div>
         </div>
     );
