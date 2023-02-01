852602e228d07acc153baefe06ce5212b6ec19d2
singhutsav5502 <singhutsavofficial@gmail.com>
2023-02-01 22:41:22 +0530

    Make Card act as a wrapper using children reserved word

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,7 +1,8 @@
 import './Card.css'
-function Card(){
+function Card(props){
     return(
-        <div className = 'card' > </div>
+        <div className = 'card'>{props.children}</div>
     )
+
 }
 export default Card;
