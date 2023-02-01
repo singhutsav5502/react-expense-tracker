d71763439253f76f0aa62f1982262c6ba2786f5c
singhutsav5502 <singhutsavofficial@gmail.com>
2023-02-01 22:37:56 +0530

    Implemented Card Element

--- src/Components/ExpenseDate.js
+++ src/Components/ExpenseDate.js
@@ -1,14 +1,15 @@
 import "./ExpenseDate.css"
+import Card from './Card.js'
 function ExpenseDate(props) {
     const month = props.date.toLocaleString('en-US', { month: 'long' });
     const day = props.date.toLocaleString('en-US', { day: '2-digit' });
     const year = props.date.getFullYear();
     return (
-        <div className = "expense-date">
+        <Card className = "expense-date">
             <div className="expense-date__month">{month}</div>
             <div className = "expense-date__day">{day}</div>
             <div className = "expense-date__year">{year}</div>
-        </div>
+        </Card>
     )
 }
 export default ExpenseDate;
