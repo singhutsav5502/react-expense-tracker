b5cc5d1e9cfbdadec5ddb2e399d26caed4dcf77c
singhutsav5502 <singhutsavofficial@gmail.com>
2023-02-07 02:37:34 +0530

    remove redundant state

--- src/Components/EXPENSES/ExpenseItem.js
+++ src/Components/EXPENSES/ExpenseItem.js
@@ -3,17 +3,11 @@ import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate.js';
 import Card from '../UI/Card.js'
 const ExpenseItem = (props) => {
-        const [title , setTitle] = useState(props.title); // Array destructuring
-
-    //     const clickHandler = () => {
-    //         setTitle('Updated!!')
-    //     }
-    //<button onClick={clickHandler}>Change Title</button> */
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
 
