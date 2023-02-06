903822a90548d02512e12a647b14726215f5d9ce
singhutsav5502 <singhutsavofficial@gmail.com>
2023-02-06 23:07:40 +0530

    Implement 2 Way bindin for input fields

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -66,27 +66,46 @@ const ExpenseForm = () => {
     const submitHandler = (event) => {
         event.preventDefault(); //Makes sure the page doesn't reload on form submit
         const expenseData = {
-            title:enteredTitle,
-            amount:enteredAmount,
-            date:new Date(enteredDate),
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate),
         };
 
         console.log(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
     };
     return (
         <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler} />
+
+                    <input type="text"
+                        value={enteredTitle}
+                        onChange={titleChangeHandler}
+                    />
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+                    <input
+                        type="number"
+                        min="0.01"
+                        step="0.01"
+                        value={enteredAmount}
+                        onChange={amountChangeHandler}
+                    />
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type="date" min="2021-01-01" max="2025-12-31" onChange={dateChangeHandler} />
+                    <input
+                        type="date"
+                        min="2021-01-01"
+                        max="2025-12-31"
+                        value={enteredDate}
+                        onChange={dateChangeHandler}
+                    />
                 </div>
             </div>
             <div className="new-expense__actions">
