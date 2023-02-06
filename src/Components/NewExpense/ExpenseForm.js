b6c35ed36f8cb9e4a5285524ecd5b0126e3a8c11
singhutsav5502 <singhutsavofficial@gmail.com>
2023-02-06 22:50:08 +0530

    Handle basic form submission

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -12,7 +12,7 @@ const ExpenseForm = () => {
     const titleChangeHandler = (event) => {
         setEnteredTitle(event.target.value);
         // **************************************
-         
+
         // setUserInput({
         //     ...userInput,
         //     enteredTitle: event.target.value,
@@ -63,8 +63,18 @@ const ExpenseForm = () => {
 
         // **************************************
     };
+    const submitHandler = (event) => {
+        event.preventDefault(); //Makes sure the page doesn't reload on form submit
+        const expenseData = {
+            title:enteredTitle,
+            amount:enteredAmount,
+            date:new Date(enteredDate),
+        };
+
+        console.log(expenseData);
+    };
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
