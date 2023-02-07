fd43aa7d98270524e8763b3c6d86abc7d3b60d4b
singhutsav5502 <singhutsavofficial@gmail.com>
2023-02-07 21:52:51 +0530

    Add styling to ExpenseFormToggle

--- src/Components/NewExpense/ExpenseFormToggle.js
+++ src/Components/NewExpense/ExpenseFormToggle.js
@@ -1,15 +1,24 @@
-import React , {useState} from 'react';
+import React, { useState } from 'react';
 import './ExpenseFormToggle.css';
 import NewExpense from './NewExpense';
 const ExpenseFormToggle = (props) => {
     const [visibility, setVisibility] = useState(false);
-    const onCancelButton = () => { setVisibility(false);};
-    const newExpenseHandler =()=>{setVisibility(true)};
+    const onCancelButton = () => { setVisibility(false); };
+    const newExpenseHandler = () => { setVisibility(true) };
     const onAddExpenseHandler = (expense) => {
         props.onAddExpense(expense);
+        setVisibility(false);
     }
-    if(visibility===true){return <NewExpense onCancelButton={onCancelButton} onAddExpense={onAddExpenseHandler} />}
-    else{return <button onClick={newExpenseHandler}>Add New Expense</button> };
-        
+    if (visibility === true) { return <NewExpense onCancelButton={onCancelButton} onAddExpense={onAddExpenseHandler} /> }
+    else {
+        return <div className="new-expense">
+            <div className="new-expense__controls" >
+                <div className="new-expense__actions">
+                    <button className="" onClick={newExpenseHandler}>Add New Expense</button>
+                </div>
+            </div>
+        </div>
+    };
+
 };
 export default ExpenseFormToggle;
